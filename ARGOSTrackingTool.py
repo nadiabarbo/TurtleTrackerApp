195d4f67d18377489e1456273f9d4bf9c994415f
nadiabarbo <112417252+nadiabarbo@users.noreply.github.com>
2022-10-03 13:07:05 -0400

    Update ARGOSTrackingTool.py

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -66,6 +66,33 @@ obs_lon = lineData[7]     # Observation Longitude
 #%% Implementing the new things I learned
 
 #Create a variable pointing to the data file
+##file_object = open(file=file_name,mode='r')
+
+#Read contents of file into a list
+##line_list = file_object.readlines()
+
+#Close the file
+##file_object.close()
+
+#Pretend we read one line of data from the file
+##lineString = line_list[100]
+
+#Split the string into a list of data items
+##lineData = lineString.split()
+
+#Extract items in list into variables
+##record_id = lineData[0]
+##obs_date = lineData[2]
+##obs_lc = lineData[4]
+##obs_lat = lineData[6]
+##obs_lon = lineData[7]
+
+#Print the location of sara
+##print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+
+#%% Doing the above but using a for loop to get all lines of data
+
+#Create a variable pointing to the data file
 file_name = 'data/raw/sara.txt'
 
 #Create a file object from the file
@@ -78,17 +105,52 @@ line_list = file_object.readlines()
 file_object.close()
 
 #Pretend we read one line of data from the file
-lineString = line_list[100]
+for lineString in line_list[17:]:
 
-#Split the string into a list of data items
-lineData = lineString.split()
+    #Split the string into a list of data items
+    lineData = lineString.split()
+    
+    #Extract items in list into variables
+    record_id = lineData[0]
+    obs_date = lineData[2]
+    obs_lc = lineData[4]
+    obs_lat = lineData[6]
+    obs_lon = lineData[7]
+    
+    #Print the location of sara
+    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
 
-#Extract items in list into variables
-record_id = lineData[0]
-obs_date = lineData[2]
-obs_lc = lineData[4]
-obs_lat = lineData[6]
-obs_lon = lineData[7]
+#%% Another way to do thing above
 
-#Print the location of sara
-print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+#Create a variable pointing to the data file
+file_name = 'data/raw/sara.txt'
+
+#Create a file object from the file
+file_object = open(file=file_name,mode='r')
+
+#Read contents of file into a list
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
+
+#Pretend we read one line of data from the file
+for lineString in line_list:
+    
+    #check to see if the linestring is a data line
+    if lineString[0] in ('#','u'):
+        continue
+
+    #Split the string into a list of data items
+    lineData = lineString.split()
+    
+    #Extract items in list into variables
+    record_id = lineData[0]
+    obs_date = lineData[2]
+    obs_lc = lineData[4]
+    obs_lat = lineData[6]
+    obs_lon = lineData[7]
+    
+    #Print the location of sara
+    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+
