34a8d55a692c269c74d4f234dd419bc523f4d7d1
nadiabarbo <112417252+nadiabarbo@users.noreply.github.com>
2022-10-03 13:15:33 -0400

    Update ARGOSTrackingTool.py

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -154,3 +154,40 @@ for lineString in line_list:
     #Print the location of sara
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
 
+#%% loading in all the data using a while loop - so it only saves one line at a time
+
+#Create a variable pointing to the data file
+file_name = 'data/raw/sara.txt'
+
+#Create a file object from the file
+file_object = open(file=file_name,mode='r')
+
+#Read one line at a time of contents of file into a list
+lineString = file_object.readline()
+
+#Extract one data line into a variable
+while lineString:
+    
+    #check to see if the linestring is a data line
+    if lineString[0] in ('#','u'):
+        lineString = file_object.readline()
+        continue
+
+    #Split the string into a list of data items
+    lineData = lineString.split()
+    
+    #Extract items in list into variables
+    record_id = lineData[0]
+    obs_date = lineData[2]
+    obs_lc = lineData[4]
+    obs_lat = lineData[6]
+    obs_lon = lineData[7]
+    
+    #Print the location of sara
+    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+    
+    #move to the next line in the file
+    lineString = file_object.readline()
+    
+#close the file object
+file_object.close()
