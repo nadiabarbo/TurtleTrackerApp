e927f6d8bbc3544cd38f12d42fc4df6519a7bfc8
nadiabarbo <112417252+nadiabarbo@users.noreply.github.com>
2022-10-12 12:30:40 -0400

    Update ARGOSTrackingTool.py

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -93,32 +93,32 @@ obs_lon = lineData[7]     # Observation Longitude
 #%% Doing the above but using a for loop to get all lines of data
 
 #Create a variable pointing to the data file
-file_name = 'data/raw/sara.txt'
+##file_name = 'data/raw/sara.txt'
 
 #Create a file object from the file
-file_object = open(file=file_name,mode='r')
+##file_object = open(file=file_name,mode='r')
 
 #Read contents of file into a list
-line_list = file_object.readlines()
+##line_list = file_object.readlines()
 
 #Close the file
-file_object.close()
+##file_object.close()
 
 #Pretend we read one line of data from the file
-for lineString in line_list[17:]:
+##for lineString in line_list[17:]:
 
     #Split the string into a list of data items
-    lineData = lineString.split()
+   ## lineData = lineString.split()
     
     #Extract items in list into variables
-    record_id = lineData[0]
-    obs_date = lineData[2]
-    obs_lc = lineData[4]
-    obs_lat = lineData[6]
-    obs_lon = lineData[7]
+   ## record_id = lineData[0]
+   ## obs_date = lineData[2]
+   ## obs_lc = lineData[4]
+   ## obs_lat = lineData[6]
+   ## obs_lon = lineData[7]
     
     #Print the location of sara
-    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+   ## print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
 
 #%% Another way to do thing above
 
@@ -133,6 +133,10 @@ line_list = file_object.readlines()
 
 #Close the file
 file_object.close()
+
+#creating empty dictionaries
+date_dict = {}
+location_dict = {}
 
 #Pretend we read one line of data from the file
 for lineString in line_list:
@@ -151,43 +155,51 @@ for lineString in line_list:
     obs_lat = lineData[6]
     obs_lon = lineData[7]
     
+    
+    #add items to dictionaries, if lc criteria is met
+    if obs_lc in ("1","2","3"):
+        date_dict[record_id] = obs_date
+        location_dict[record_id] = (obs_lat,obs_lon)
+    
     #Print the location of sara
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
 
 #%% loading in all the data using a while loop - so it only saves one line at a time
 
+
+
 #Create a variable pointing to the data file
-file_name = 'data/raw/sara.txt'
+##file_name = 'data/raw/sara.txt'
 
 #Create a file object from the file
-file_object = open(file=file_name,mode='r')
+##file_object = open(file=file_name,mode='r')
 
 #Read one line at a time of contents of file into a list
-lineString = file_object.readline()
+##lineString = file_object.readline()
 
 #Extract one data line into a variable
-while lineString:
+##while lineString:
     
     #check to see if the linestring is a data line
-    if lineString[0] in ('#','u'):
-        lineString = file_object.readline()
-        continue
+   ## if lineString[0] in ('#','u'):
+        ##lineString = file_object.readline()
+       ## continue
 
     #Split the string into a list of data items
-    lineData = lineString.split()
+    ##lineData = lineString.split()
     
     #Extract items in list into variables
-    record_id = lineData[0]
-    obs_date = lineData[2]
-    obs_lc = lineData[4]
-    obs_lat = lineData[6]
-    obs_lon = lineData[7]
+   ## record_id = lineData[0]
+   ## obs_date = lineData[2]
+   ## obs_lc = lineData[4]
+   ## obs_lat = lineData[6]
+   ## obs_lon = lineData[7]
     
     #Print the location of sara
-    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+   ## print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
     
     #move to the next line in the file
-    lineString = file_object.readline()
+    ##lineString = file_object.readline()
     
 #close the file object
-file_object.close()
+##file_object.close()
